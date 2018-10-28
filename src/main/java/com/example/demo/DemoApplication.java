59d1e0ead3974928d54cb326e1ee64e0feea96bf
zhongwu.zhang <zhongwu.zhang@xiaobao100.com>
2018-10-28 14:40:44 +0800

    project init

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@SpringBootApplication
+
+public class DemoApplication {
+    @RequestMapping("/hello")
+    String index(){
+        return "hello spring boot";
+    }
+
+//    public static void main(String[] args) {
+//        SpringApplication.run(DemoApplication.class, args);
+//    }
+}
